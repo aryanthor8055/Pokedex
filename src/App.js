a5accea16571da6a7429f5d7e7211b70e63184a3
Aryan <aryan16072001236809@gmail.com>
2022-10-25 19:30:16 +0530

    Adding Pokegame Component and Styling the App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import Pokecard from './Pokecard';
-import Pokedex from './Pokedex';
+import Pokegame from './Pokegame';
 
 function App() {
   return (
     <div className="App">
-      <Pokedex/>
+      <Pokegame/>
     </div>
   
   );
